a6aa912acf6b5b5fc2838f32adc5b426a394f727
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-12-24 16:13:08 +0300

    change method to optional page from list, add a method to find character by name and description
    Signed-off-by: Stanislav Stoianov <stanis.stoyanov@gmail.com>

--- src/main/java/io/ssstoyanov/marvel/repositories/CharacterRepository.java
+++ src/main/java/io/ssstoyanov/marvel/repositories/CharacterRepository.java
@@ -1,14 +1,17 @@
 package io.ssstoyanov.marvel.repositories;
 
 import io.ssstoyanov.marvel.entities.Character;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CharacterRepository extends MongoRepository<Character, Long> {
-    List<Character> findCharactersByComicsId(Long id, Pageable pageable);
+    Optional<Page<Character>> findCharactersByComicsId(Long id, Pageable pageable);
+
+    Optional<Character> findByNameAndDescriptionContains(String name, String description);
 }
 

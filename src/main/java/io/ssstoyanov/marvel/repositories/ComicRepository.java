50b4c572e04770889a72b24296a9bf5feccad452
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-12-24 16:12:17 +0300

    change method to optional page from list
    Signed-off-by: Stanislav Stoianov <stanis.stoyanov@gmail.com>

--- src/main/java/io/ssstoyanov/marvel/repositories/ComicRepository.java
+++ src/main/java/io/ssstoyanov/marvel/repositories/ComicRepository.java
@@ -1,13 +1,14 @@
 package io.ssstoyanov.marvel.repositories;
 
 import io.ssstoyanov.marvel.entities.Comic;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ComicRepository extends MongoRepository<Comic, Long> {
-    List<Comic> findComicsByCharactersId(Long id, Pageable page);
+    Optional<Page<Comic>> findComicsByCharactersId(Long id, Pageable page);
 }
